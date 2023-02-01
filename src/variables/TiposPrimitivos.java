84cd19de0f01d9b345015b9552643a60b29e10ff
Alejandro Garcia Marchena <agarciamar@ayesa.es>
2023-02-01 17:32:44 +0100

    esto es una prueba

---
+++ src/variables/TiposPrimitivos.java
@@ -0,0 +1,34 @@
+package variables;
+
+/**
+ * 
+ * @author agarciamar
+ *
+ */
+
+/**
+ * 
+ * Esta clase es para mostrar como funcionan los tipos primitivos de JAva
+ *
+ */
+public class TiposPrimitivos {
+
+	/**
+	 * Metodo main autogenerado
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte var1=55;
+		byte var2=127;
+		byte var3=-34;
+		byte var4=(byte) 256;
+		
+		System.out.println("var1: "+var1);
+		System.out.println("var2: "+var2);
+		System.out.println("var3"+var3);
+		System.out.println("var4: "+var4);
+	}
+
+}
